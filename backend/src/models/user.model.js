5ab1e2a51243ed5bd02f967d62ea7b3e2d422740
sreegopalsaha <sreegopal0101@gmail.com>
2024-11-16 22:29:13 +0530

    add JWT and password hashing

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     fullname:{
@@ -55,9 +57,50 @@ const userSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         }
-    ]
-},{
+    ],
+    refreshToken: {
+        type: String
+    }
+},
+{
     timestamps: true 
 });
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(givenPassword){
+    return await bcrypt.compare(givenPassword, this.password);
+};
+
+userSchema.methods.generateAccessToken = function (){
+    const token = jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullname: this.fullname,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+    return token;
+};
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)};
 
 export default mongoose.model('User', userSchema);
