0a5f404d32f8e36be999b60ef78631947aaa2300
sreegopalsaha <sreegopal0101@gmail.com>
2025-02-10 00:10:03 +0530

    feat: add API service with authentication token handling for user login and registration

---
+++ frontend/src/services/ApiServices.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import Cookies from 'js-cookie';
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use((config) => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const loginUser = (data) => api.post('/user/login', data);
+export const registerUser = (data) => api.post('/user/register', data);
