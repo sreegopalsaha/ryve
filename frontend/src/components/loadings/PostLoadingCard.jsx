564fc507c32e105a1923000ca744fd884d7e7783
sreegopalsaha <sreegopal0101@gmail.com>
2025-02-12 23:42:37 +0530

    fix folder name

---
+++ frontend/src/components/loadings/PostLoadingCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "../molecules/Card";
+
+function PostLoadingCard({ className = "" }) {
+  return (
+    <Card className={`w-full animate-pulse p-4 space-y-4 ${className}`}>
+      <div className="flex items-center gap-3">
+        <div className="w-10 h-10 skeleton"></div>
+        <div className="flex flex-col gap-2">
+          <div className="w-24 h-4 skeleton"></div>
+          <div className="w-16 h-3 skeleton"></div>
+        </div>
+      </div>
+
+      <div className="w-full h-16 skeleton"></div>
+
+      <div className="w-full h-40 skeleton"></div>
+
+      <div className="flex justify-between items-center">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="w-10 h-6 skeleton"></div>
+        ))}
+      </div>
+    </Card>
+  );
+}
+
+const PostLoading = () => <PostLoadingCard className="h-[70%]" />;
+
+const PostsLoading = () => (
+  <>
+    {[...Array(5)].map((_, index) => (
+      <PostLoadingCard key={index} />
+    ))}
+  </>
+);
+
+export { PostLoading, PostLoadingCard, PostsLoading };
