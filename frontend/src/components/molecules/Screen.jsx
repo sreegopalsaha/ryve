e01820969cb67d87f93691ad4b0546084f1739c5
sreegopalsaha <sreegopal0101@gmail.com>
2025-02-14 00:14:09 +0530

    improve UI

--- frontend/src/components/molecules/Screen.jsx
+++ frontend/src/components/molecules/Screen.jsx
@@ -1,8 +1,8 @@
 function Screen({ children, className = "", middleScreen }) {
   return (
     <div
-      className={`min-h-screen h-full flex flex-col p-5 theme-background theme-text 
-      ${middleScreen ? "w-full pb-16 md:pb-5 md:flex-1 md:ml-[25%] md:mr-[30%] md:border-x md:border-x-black md:dark:border-x-white md:border-y-0" : ""}
+      className={`flex flex-col min-h-screen h-full pb-16 md:pb-5 px-1 pt-5 md:p-5 theme-background theme-text 
+      ${middleScreen ? "w-full md:flex-1 md:ml-[25%] md:mr-[30%] md:border-x md:border-x-black md:dark:border-x-white" : ""}
       ${className}`}
     >
       {children}
